5b2bec0dc735eca9507fcce11bda1485b074e754
Mori <mori_morou@rocketmail.com>
2019-06-20 18:08:51 +0300

    added

--- taco-cloud/src/main/java/ru/morou/tacocloud/data/TacoRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.morou.tacocloud.data;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.morou.tacocloud.Taco;
-
-public interface TacoRepository extends CrudRepository<Taco, Long> {
-
-}
